479fcd8348ec67845e7827e89ce679cb76841d0b
Jony Liu <liu_zengqiang@126.com>
2019-07-29 13:51:03 +0800

    add jmap test

--- src/main/java/com/example/algorithm/gc/GcTest.java
+++ src/main/java/com/example/algorithm/gc/GcTest.java
@@ -1,15 +1,21 @@
 package com.example.algorithm.gc;
 
+/**
+ * check the jvm:
+ * jps
+ * jmap -dump:format=b,file=heap.bin <pid>
+ * jhat -J-Xmx512m heap.bin
+ * http://localhost:7000
+ */
 public class GcTest {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         for (int i = 0; i < 4; i++) {
-            new GcTest();
-            // System.gc();
+            BigObject bigObject = new BigObject();
+            System.out.println(bigObject.toString());
+            //System.gc();
             Runtime.getRuntime().gc();
-
         }
-
     }
 
     public void finalize() {
