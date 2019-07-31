9cbf6878f2aada58d92077e4a551bad5d4c7d824
Jony Liu <liu_zengqiang@126.com>
2019-07-31 20:45:51 +0800

    add circle demo

---
+++ src/main/java/com/example/algorithm/circle/Demo.java
@@ -0,0 +1,35 @@
+package com.example.algorithm.circle;
+
+import org.junit.Test;
+
+public class Demo {
+
+    @Test
+    public void test03() {
+        int[] a = {2, 5, 7, 3, 6};
+        //for-earch循环，写法简洁，但不能用下标动态的访问数组
+        for (int k : a) {
+            System.out.print(k + " ");
+        }
+    }
+
+    @Test
+    public void test02() {
+        int count = 0;
+        int[] a = {2};
+        do {
+            System.out.print(a[count] + " ");
+        } while (count++ < a.length-1);
+    }
+
+    @Test
+    public void test01() {
+        int count = 0;
+        int[] a = {2, 5, 7, 3, 6};
+        while (count < a.length) {
+            System.out.print(a[count] + " ");
+            count++;
+        }
+
+    }
+}
