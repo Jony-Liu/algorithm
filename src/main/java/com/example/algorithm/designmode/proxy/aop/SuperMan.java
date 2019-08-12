d4fd31aba5d2f06bef67e93de513ecf38e6e46f1
Jony Liu <liu_zengqiang@126.com>
2019-08-12 16:50:59 +0800

    add aop proxy demo

---
+++ src/main/java/com/example/algorithm/designmode/proxy/aop/SuperMan.java
@@ -0,0 +1,17 @@
+package com.example.algorithm.designmode.proxy.aop;
+
+public class SuperMan implements Human {
+
+    @Override
+    public void info() {
+        // TODO Auto-generated method stub
+        System.out.println("I am SuperMan");
+    }
+
+    @Override
+    public void fly() {
+        // TODO Auto-generated method stub
+        System.out.println("I can fly");
+    }
+
+}
