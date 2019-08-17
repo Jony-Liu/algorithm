f8e7744ca4ed57402cb861776951bf534c031ea0
Jony Liu <liu_zengqiang@126.com>
2019-08-17 19:48:04 +0800

    add lock demo

---
+++ src/main/java/com/example/algorithm/lock/TestThread.java
@@ -0,0 +1,12 @@
+package com.example.algorithm.lock;
+
+public class TestThread {
+
+    static Account account = new Account("123", 0);
+
+    public static void main(String[] args) {
+        new DramThread(account, 100).start();
+        new DepositThread(account, 100).start();
+    }
+
+}
