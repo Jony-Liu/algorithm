666a17f6f6516ee8618e2f63253182a31535e12b
Jony Liu <liu_zengqiang@126.com>
2019-09-30 22:11:10 +0800

    add a test demo

---
+++ src/main/java/com/example/algorithm/demo/BaseTypeTest.java
@@ -0,0 +1,39 @@
+package com.example.algorithm.demo;
+
+import org.junit.Test;
+
+/**
+ * @author Jony-Liu
+ * @date 2019/9/30 22:03
+ */
+public class BaseTypeTest {
+
+    static byte b;
+
+    static short s;
+
+    static int i;
+
+    static long l;
+
+    static float f;
+
+    static double d;
+
+    static char c;
+
+    static boolean bl;
+
+    @Test
+    public void Test01(){
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bl);
+    }
+
+}
