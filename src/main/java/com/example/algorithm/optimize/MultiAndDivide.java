7bafc8011e5ac68640c09718a966eb830d43283c
Jony Liu <51904223+Jony-Liu@users.noreply.github.com>
2019-12-13 09:48:18 +0800

    Create MultiAndDivide.java

---
+++ src/main/java/com/example/algorithm/optimize/MultiAndDivide.java
@@ -0,0 +1,36 @@
+package com.example.algorithm.optimize;
+
+import org.junit.Test;
+
+/**
+ * @author jonyliu
+ * @date 2019/12/13 9:11
+ */
+public class MultiAndDivide {
+
+    @Test
+    public void test() {
+        int val;
+        int a = 0;
+        int b = 0;
+        for (val = 0; val < 100000; val += 5) {
+            a = val * 8;
+            b = val / 2;
+        }
+        System.out.println(a);
+        System.out.println(b);
+    }
+    @Test
+    public void test01() {
+        int val;
+        int a = 0;
+        int b = 0;
+        for (val = 0; val < 100000; val += 5) {
+            a = val << 3;
+            b = val >> 1;
+        }
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
