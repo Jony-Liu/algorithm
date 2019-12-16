bb0b391ee5c03a7b5f1eb9a8ce66e5af8cb0d208
Jony Liu <51904223+Jony-Liu@users.noreply.github.com>
2019-12-16 19:47:35 +0800

    Create ArrayDemo.java

---
+++ src/main/java/com/example/algorithm/optimize/ArrayDemo.java
@@ -0,0 +1,23 @@
+package com.example.algorithm.optimize;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jonyliu
+ * @date 2019/12/16 9:33
+ */
+public class ArrayDemo {
+    @Test
+    public void test(){
+        String[] strings = {"a","b","c","d"};
+        System.out.println(strings.toString());
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list.toString());
+    }
+}
