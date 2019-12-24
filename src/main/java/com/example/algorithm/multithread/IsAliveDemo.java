d24199a08438fecd5c19fb6b248b4053a5299fd8
Jony Liu <51904223+Jony-Liu@users.noreply.github.com>
2019-12-24 09:23:25 +0800

    Create IsAliveDemo.java

---
+++ src/main/java/com/example/algorithm/multithread/IsAliveDemo.java
@@ -0,0 +1,27 @@
+package com.example.algorithm.multithread;
+
+/**
+ * @author jonyliu
+ * @date 2019/12/24 9:07
+ */
+public class IsAliveDemo extends Thread{
+
+    public IsAliveDemo(){
+        System.out.println("IsAliveDemo--begin");
+        System.out.println("Thread.currentThread().getName()="+Thread.currentThread().getName());
+        System.out.println("this.getName()="+this.getName());
+        System.out.println("Thread.currentThread().isAlive() ="+Thread.currentThread().isAlive());
+        System.out.println("this.isAlive()="+this.isAlive());
+        System.out.println("IsAliveDemo--end");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("run--begin");
+        System.out.println("Thread.currentThread().getName()="+Thread.currentThread().getName());
+        System.out.println("this.getName()="+this.getName());
+        System.out.println("Thread.currentThread().isAlive() ="+Thread.currentThread().isAlive());
+        System.out.println("this.isAlive()="+this.isAlive());
+        System.out.println("run--end");
+    }
+}
