ad10e8ca04c3d1c87030f23ef5e472ddd9e7068c
Jony Liu <51904223+Jony-Liu@users.noreply.github.com>
2019-12-24 09:23:52 +0800

    Create IsAliveTest.java

---
+++ src/main/java/com/example/algorithm/multithread/IsAliveTest.java
@@ -0,0 +1,16 @@
+package com.example.algorithm.multithread;
+
+/**
+ * @author jonyliu
+ * @date 2019/12/24 9:09
+ */
+public class IsAliveTest {
+    public static void main(String[] args) {
+        IsAliveDemo isAliveDemo = new IsAliveDemo();
+        Thread thread = new Thread(isAliveDemo);
+        thread.setName("A");
+        System.out.println("isAliveDemo isAlive== "+thread.isAlive());
+        thread.start();
+        System.out.println("isAliveDemo isAlive== "+thread.isAlive());
+    }
+}
