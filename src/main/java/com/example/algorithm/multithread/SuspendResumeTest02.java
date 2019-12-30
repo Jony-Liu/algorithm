f650b3cec98dc8dadf809aa663fb85e3c238a751
Jony Liu <51904223+Jony-Liu@users.noreply.github.com>
2019-12-30 10:32:06 +0800

    Create SuspendResumeTest02.java

---
+++ src/main/java/com/example/algorithm/multithread/SuspendResumeTest02.java
@@ -0,0 +1,28 @@
+package com.example.algorithm.multithread;
+
+/**
+ * @author jonyliu
+ * @date 2019/12/30 9:38
+ */
+public class SuspendResumeTest02 {
+    public static void main(String[] args) throws Exception {
+        SuspendResumeDemo02 suspendResumeDemo02 = new SuspendResumeDemo02();
+        Thread thread01 = new Thread() {
+            @Override
+            public void run() {
+                suspendResumeDemo02.printString();
+            }
+        };
+        thread01.setName("a");
+        thread01.start();
+        Thread.sleep(1000);
+        Thread thread02 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("无法进入printString()");
+                suspendResumeDemo02.printString();
+            }
+        };
+        thread02.start();
+    }
+}
