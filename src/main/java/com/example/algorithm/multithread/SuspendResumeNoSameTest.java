4672f3cf06240f9b02e374f3bda5e96bc805ddcd
Jony Liu <51904223+Jony-Liu@users.noreply.github.com>
2019-12-31 09:07:23 +0800

    Create SuspendResumeNoSameTest.java

---
+++ src/main/java/com/example/algorithm/multithread/SuspendResumeNoSameTest.java
@@ -0,0 +1,28 @@
+package com.example.algorithm.multithread;
+
+/**
+ * @author jonyliu
+ * @date 2019/12/30 21:25
+ */
+public class SuspendResumeNoSameTest {
+    public static void main(String[] args) throws Exception {
+        SuspendResumeNoSameDemo myobject = new SuspendResumeNoSameDemo();
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                myobject.setValue("a","aa");
+            }
+        };
+        thread.setName("a");
+        thread.start();
+        Thread.sleep(500);
+        Thread thread1 = new Thread(){
+            @Override
+            public void run() {
+                myobject.printUserAndPassWord();
+            }
+        };
+
+        thread1.start();
+    }
+}
