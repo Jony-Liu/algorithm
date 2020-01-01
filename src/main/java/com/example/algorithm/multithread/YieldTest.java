1ff7fbdaeb58d385ddd351cd66a016d72815fb78
Jony Liu <liu_zengqiang@126.com>
2020-01-01 20:21:28 +0800

    add YieldTest

---
+++ src/main/java/com/example/algorithm/multithread/YieldTest.java
@@ -0,0 +1,12 @@
+package com.example.algorithm.multithread;
+
+/**
+ * @author Jony-Liu
+ * @date 2020/1/1 20:19
+ */
+public class YieldTest {
+    public static void main(String[] args) {
+        YieldDemo thread = new YieldDemo();
+        thread.start();
+    }
+}
