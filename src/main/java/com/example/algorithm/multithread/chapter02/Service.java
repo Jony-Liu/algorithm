f047ca84249214448037cb0413a78cc425fe5476
Jony Liu <liu_zengqiang@126.com>
2020-01-13 21:28:03 +0800

    optimize code

--- src/main/java/com/example/algorithm/multithread/chapter02/Service.java
+++ src/main/java/com/example/algorithm/multithread/chapter02/Service.java
@@ -1,6 +1,10 @@
 package com.example.algorithm.multithread.chapter02;
 
 public class Service {
+    /**
+     * synchronized 锁重入：在使用 synchronized 时，当一个线程得到一个线程锁后，再次请求此对象时
+     * 是可以再次得到这个对象的锁的。
+     */
     synchronized public void  service1(){
         System.out.println("service1");
         service2();
---
+++ src/main/java/com/example/algorithm/multithread/chapter02/TaskTest.java
@@ -0,0 +1,15 @@
+package com.example.algorithm.multithread.chapter02;
+
+/**
+ * @author Jony-Liu
+ * @date: 2020/1/13 21:26
+ */
+public class TaskTest {
+    public static void main(String[] args) {
+        Task task = new Task();
+        TaskMethod1 taskMethod1 = new TaskMethod1(task);
+        TaskMethod2 taskMethod2 = new TaskMethod2(task);
+        taskMethod1.start();
+        taskMethod2.start();
+    }
+}
