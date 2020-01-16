830324c92688abdc75b51cda00ceee37ab62963b
Jony Liu <liu_zengqiang@126.com>
2020-01-16 20:17:49 +0800

    optimize code

--- src/main/java/com/example/algorithm/multithread/chapter02/MyThread.java
+++ src/main/java/com/example/algorithm/multithread/chapter02/MyThread.java
@@ -1,6 +1,10 @@
 package com.example.algorithm.multithread.chapter02;
 
-public class MyThread extends  Thread {
+/**
+ * @author Jony-Liu
+ * @date 2020/1/5 16:24
+ */
+public class MyThread extends Thread {
     @Override
     public void run() {
         Service service = new Service();
