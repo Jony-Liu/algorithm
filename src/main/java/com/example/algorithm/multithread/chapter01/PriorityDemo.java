c8ec52b82f48d9fa99139c9b6d35ef6c30433c6e
Jony Liu <liu_zengqiang@126.com>
2020-01-17 21:11:32 +0800

    optimize code

--- src/main/java/com/example/algorithm/multithread/chapter01/PriorityDemo.java
+++ src/main/java/com/example/algorithm/multithread/chapter01/PriorityDemo.java
@@ -1,17 +1,21 @@
 package com.example.algorithm.multithread.chapter01;
 
-public class PriorityDemo extends Thread{
+/**
+ * @author Jony-Liu
+ * @date 2019/12/22 18:52
+ */
+public class PriorityDemo extends Thread {
     @Override
     public void run() {
-        System.out.println("PriorityDemo's Priority: "+this.getPriority());
+        System.out.println("PriorityDemo's Priority: " + this.getPriority());
         PriorityDemo02 priorityDemo02 = new PriorityDemo02();
         priorityDemo02.start();
     }
 }
 
-class PriorityDemo02 extends Thread{
+class PriorityDemo02 extends Thread {
     @Override
     public void run() {
-        System.out.println("PriorityDemo02's Priority: "+this.getPriority());
+        System.out.println("PriorityDemo02's Priority: " + this.getPriority());
     }
 }
