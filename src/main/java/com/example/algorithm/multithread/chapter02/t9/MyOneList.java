05397e63877863a5922eb4fa153f6280c9462d31
Jony Liu <liu_zengqiang@126.com>
2020-01-24 14:41:42 +0800

    optimize code

---
+++ src/main/java/com/example/algorithm/multithread/chapter02/t9/MyOneList.java
@@ -0,0 +1,21 @@
+package com.example.algorithm.multithread.chapter02.t9;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jonyliu
+ * @date 2020/1/20 16:55
+ */
+public class MyOneList {
+
+    private List list = new ArrayList();
+
+    synchronized public void  add(String data){
+        list.add(data);
+    }
+    synchronized public int getSize(){
+        return list.size();
+    }
+}
