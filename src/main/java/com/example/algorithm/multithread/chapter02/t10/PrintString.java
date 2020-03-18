924d0cd80883952826aecb57d3367cd3f8bd7929
Jony Liu <liu_zengqiang@126.com>
2020-03-18 21:52:20 +0800

    add while demo

---
+++ src/main/java/com/example/algorithm/multithread/chapter02/t10/PrintString.java
@@ -0,0 +1,35 @@
+package com.example.algorithm.multithread.chapter02.t10;
+
+/**
+ * @author Jony-Liu
+ * @date: 2020/3/18 21:24
+ */
+public class PrintString implements Runnable {
+
+    private boolean isContinuePrint = true;
+
+    public boolean isContinuePrint() {
+        return isContinuePrint;
+    }
+
+    public void setContinuePrint(boolean isContinuePrint) {
+        this.isContinuePrint = isContinuePrint;
+    }
+
+    public void printStringMethod() {
+        try {
+            while (isContinuePrint == true) {
+                System.out.println("run printStringMethod threadName=" + Thread.currentThread().getName());
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        printStringMethod();
+    }
+
+}
---
+++ src/main/java/com/example/algorithm/multithread/chapter02/t10/Run.java
@@ -0,0 +1,15 @@
+package com.example.algorithm.multithread.chapter02.t10;
+
+/**
+ * @author Jony-Liu
+ * @date: 2020/3/18 21:28
+ */
+public class Run {
+    public static void main(String[] args)  throws Exception{
+        PrintString printString = new PrintString();
+        new Thread(printString).start();
+        System.out.println("我要停止它！ stopThread = "+Thread.currentThread().getName());
+        printString.setContinuePrint(false);
+        System.out.println(printString.isContinuePrint());
+    }
+}
