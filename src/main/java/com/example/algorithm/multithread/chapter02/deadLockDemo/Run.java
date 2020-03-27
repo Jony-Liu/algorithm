dbb9f50a9adacbfba35b6c28a8a4e90f5c4d8df1
Jony Liu <liu_zengqiang@126.com>
2020-03-27 19:54:07 +0800

    interrupt wait demo

--- src/main/java/com/example/algorithm/multithread/chapter02/deadLockDemo/Run.java
+++ src/main/java/com/example/algorithm/multithread/chapter02/deadLockDemo/Run.java
@@ -2,7 +2,7 @@ package com.example.algorithm.multithread.chapter02.deadLockDemo;
 
 /**
  * @author Jony-Liu
- * @date: 2020/2/17 11:17
+ * @date 2020/2/17 11:17
  */
 public class Run {
     public static void main(String[] args) {
