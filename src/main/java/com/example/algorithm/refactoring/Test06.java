850a49cba90bec163bed6e07166a9fb1f068b2ff
Jony Liu <liu_zengqiang@126.com>
2020-06-16 22:04:56 +0800

    optimize code

---
+++ src/main/java/com/example/algorithm/refactoring/Test06.java
@@ -0,0 +1,37 @@
+package com.example.algorithm.refactoring;
+
+/**
+ * @author Jony-Liu
+ * @date 2020/6/16 22:04
+ */
+public class Test06 {
+}
+class BadExample06 {
+
+    public void someMethod(int i,int j,int k,int l,int m,int n){
+        //code
+    }
+
+}
+
+/* ---------------------分割线---------------------- */
+
+class GoodExample06 {
+
+    public void someMethod(Data data){
+        //code
+    }
+
+}
+
+class Data{
+
+    private int i;
+    private int j;
+    private int k;
+    private int l;
+    private int m;
+    private int n;
+
+
+}
