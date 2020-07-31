f4bb4507f6a56c9f2d2570ebc793fb4de2e6623c
Jony Liu <liu_zengqiang@126.com>
2020-07-31 22:51:46 +0800

    optimize code

--- src/main/java/com/example/algorithm/exception/TryCatchTest8.java
+++ src/main/java/com/example/algorithm/exception/TryCatchTest8.java
@@ -5,6 +5,13 @@ package com.example.algorithm.exception;
  * @date 2020/6/3 21:09
  */
 public class TryCatchTest8 {
+
+    /**
+     * TryCatchTest6,TryCatchTest7,TryCatchTest8中说明了一点，在分析含有finally块的方法返回值时，要对于return出现的地方进行
+     * 具体分析。在finally块中进行return操作的话，则方法整体的返回值就是finally块中的return返回值。如果在finally块之后的方法
+     * 内return，则return的值就是进行完上面的操作后的return值。
+     * @return
+     */
     private static int total() {
         int i = 1;
         try {
@@ -19,4 +26,5 @@ public class TryCatchTest8 {
     public static void main(String[] args) {
         System.out.println("执行main：" + total());
     }
+
 }
