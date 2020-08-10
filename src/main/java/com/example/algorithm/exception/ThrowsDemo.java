7e0e76f8cc62de2ac13298c785fee43a6c3f7a8b
Jony Liu <liu_zengqiang@126.com>
2020-08-10 22:59:50 +0800

    optimize code

--- src/main/java/com/example/algorithm/exception/ThrowsDemo.java
+++ src/main/java/com/example/algorithm/exception/ThrowsDemo.java
@@ -10,6 +10,7 @@ public class ThrowsDemo {
         int b= 0;
         System.out.println(a/b);
     }
+
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         try{
