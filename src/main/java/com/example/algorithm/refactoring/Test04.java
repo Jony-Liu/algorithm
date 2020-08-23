2fb6b625af74d9060e8cc9195d8f71c30eaddb14
Jony Liu <liu_zengqiang@126.com>
2020-08-23 21:27:38 +0800

    optimize code

--- src/main/java/com/example/algorithm/refactoring/Test04.java
+++ src/main/java/com/example/algorithm/refactoring/Test04.java
@@ -3,6 +3,7 @@ package com.example.algorithm.refactoring;
 /**
  * @author Jony-Liu
  * @date 2020/6/11 22:41
+ * 嵌套条件分支的优化（2）
  */
 public class Test04 {
     public static void main(String[] args) {
@@ -28,6 +29,11 @@ class BadExample04 {
 
 class GoodExample04 {
 
+    /**
+     * 将条件分支合并，以此来达到代码清晰的目的
+     * @param A
+     * @param B
+     */
     public void someMethod(Object A,Object B){
         if (A != null && B != null) {
             //code
