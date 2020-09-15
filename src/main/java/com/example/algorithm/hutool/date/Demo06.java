b69ec49894b702c94df8325c37fda01c7d7d8233
Jony Liu <liu_zengqiang@126.com>
2020-09-15 22:05:35 +0800

    optimize code

---
+++ src/main/java/com/example/algorithm/hutool/date/Demo06.java
@@ -0,0 +1,28 @@
+package com.example.algorithm.hutool.date;
+
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+
+import java.util.Date;
+
+/**
+ * @author Jony-Liu
+ * @date 2020/9/15 22:04
+ * 日期时间偏移
+ */
+public class Demo06 {
+    public static void main(String[] args) {
+        String dateStr = "2017-03-01 22:33:23";
+        Date date = DateUtil.parse(dateStr);
+
+        //结果：2017-03-03 22:33:23
+        Date newDate = DateUtil.offset(date, DateField.DAY_OF_MONTH, 2);
+
+        //常用偏移，结果：2017-03-04 22:33:23
+        DateTime newDate2 = DateUtil.offsetDay(date, 3);
+
+        //常用偏移，结果：2017-03-01 19:33:23
+        DateTime newDate3 = DateUtil.offsetHour(date, -3);
+    }
+}
